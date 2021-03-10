0151e69fa94b5be042fef47bcb90fc0315d373aa
Laura Hantz <laura.hantz@hotmail.fr>
2021-03-10 20:45:14 +0100

    test unitaires

---
+++ tests/Service/MySluggerTest.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace tests\Service;
+
+
+use App\Service\MySlugger;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+
+
+class MySluggerTest extends KernelTestCase   
+{
+    public function testSlugify()
+    {
+        self::bootKernel();
+
+        //$symfonySlugger = new AsciiSlugger();
+        //$mySlugger = new MySlugger($symfonySlugger, false);
+
+        $container = self::$container;
+
+        $mySlugger = $container->get(MySlugger::class);
+
+
+        $this->assertEquals(
+            'cours-forrest-cours',
+            $mySlugger->slugify('cours forrest cours')
+        );
+        
+
+    
+    }
+}
